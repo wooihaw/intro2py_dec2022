4381b2b868b12fa2c3e25f03725134256a4e3fd5
wooihaw <wooihaw.tan@gmail.com>
2022-12-06 08:59:44 +0800

    First commit

---
+++ exercises/string_exercises/03_number.py
@@ -0,0 +1,10 @@
+# Write a Python script to ask for a number and a thing name, 
+# and print a grmmatically correct sentence describing the number of things.
+# E.g.
+# Enter a number: 5
+# Enter a thing name: python
+# There are 5 pythons.
+#
+# Enter a number: 1
+# Enter a thing name: apple
+# There is 1 apple.
