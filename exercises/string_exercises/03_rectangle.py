38288eadbde93363b5b88301297eb6e0643da643
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 15:53:19 +0800

    gitautosync update 21

--- exercises/string_exercises/03_rectangle.py
+++ exercises/string_exercises/03_rectangle.py
@@ -5,12 +5,14 @@
 # Enter width: 3
 # perimeter = 16, area = 15.
 
-try:
-    length = float(input("Enter the length: "))
-    width = float(input("Enter the width: "))
-except Exception as e:
-    print(f'Error: {e}')
-else:
-    perimeter = 2*length + 2*width
-    area = length * width
-    print(f"{perimeter = }, {area = }")
+while True:
+    try:
+        length = float(input("Enter the length: "))
+        width = float(input("Enter the width: "))
+    except Exception as e:
+        print(f'Error: {e}')
+    else:
+        perimeter = 2*length + 2*width
+        area = length * width
+        print(f"{perimeter = }, {area = }")
+        break
