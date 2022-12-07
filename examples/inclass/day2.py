894e32dd68830429089230b286defb7275e237da
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 10:27:46 +0800

    gitautosync update 6

--- examples/inclass/day2.py
+++ examples/inclass/day2.py
@@ -4,3 +4,7 @@ Created on Wed Dec  7 10:24:33 2022
 
 @author: wooihaw
 """
+#%% Using get() method for dictionary
+adict = dict(a=1, b=2)
+# print(adict['c'])  # KeyError as the key 'c' cannot be found
+print(adict.get('c', 'Not found'))
