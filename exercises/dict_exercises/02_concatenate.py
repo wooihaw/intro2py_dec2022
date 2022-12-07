010ddff575a6731f14f6d5babe2641ac9489a72c
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 16:38:29 +0800

    gitautosync update 42

--- exercises/dict_exercises/02_concatenate.py
+++ exercises/dict_exercises/02_concatenate.py
@@ -14,11 +14,12 @@ for d in (d1, d2, d3):
 print(f'{d4_1=}')
 
 # Solution 2
-d4_2 = {}
-for d in (d1, d2, d3):
-    d4_2.update(d)
+d4_2 = {k: d[k] for d in (d1, d2, d3) for k in d}
 print(f'{d4_2=}')
 
 # Solution 3
-d4_3 = {k: d[k] for d in (d1, d2, d3) for k in d}
+d4_3 = {}
+for d in (d1, d2, d3):
+    d4_3.update(d)
 print(f'{d4_3=}')
+
