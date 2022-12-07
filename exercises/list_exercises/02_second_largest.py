9d147a50f89c211b5958d34c622ccba5bf8bed9c
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 16:08:16 +0800

    gitautosync update 25

--- exercises/list_exercises/02_second_largest.py
+++ exercises/list_exercises/02_second_largest.py
@@ -1,3 +1,5 @@
 # Write a Python script to find the second largest value in a list.
 alist = [12, 43, 57, 98, 83, -103, 256, -55, 185]
 print(f'{alist=}')
+
+print(f'Second largest value: {sorted(alist)[-2]}')
