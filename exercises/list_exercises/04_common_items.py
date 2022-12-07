4edd3b07e2a8d7a81207ac16dc9db63f0e7ffc7e
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 16:20:18 +0800

    gitautosync update 34

--- exercises/list_exercises/04_common_items.py
+++ exercises/list_exercises/04_common_items.py
@@ -5,3 +5,10 @@ blist = ['xyz', 2.5, 2.5, 3, 99, 99, 2.5, 'xyz', -1.2, 99]
 # Solution 1
 clist1 = list(set(alist) & set(blist))
 print(f'List of common items: {clist1}')
+
+# Solution 2
+clist2 = []
+for i in alist:
+    if i in blist:
+        clist2.append(i)
+print(f'List of common items: {clist2}')
