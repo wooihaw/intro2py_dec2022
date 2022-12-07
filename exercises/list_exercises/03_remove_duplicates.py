0defffe93638768cc0fdb3832a0debece1104ad7
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 16:14:04 +0800

    gitautosync update 30

--- exercises/list_exercises/03_remove_duplicates.py
+++ exercises/list_exercises/03_remove_duplicates.py
@@ -5,3 +5,9 @@ print(f'{alist=}')
 # Solution 1
 print(f'{list(set(alist))}')
 
+# Alternative solution
+uniq_list = []
+for i in alist:
+    if i not in uniq_list:
+        uniq_list.append(i)
+print(f'{uniq_list=}')        
