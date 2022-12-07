05c4e9782f244823759af1bb076a8ac6c8d68b48
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 16:18:45 +0800

    gitautosync update 33

--- exercises/list_exercises/04_common_items.py
+++ exercises/list_exercises/04_common_items.py
@@ -1,3 +1,7 @@
 # Write a Python script to find common items between two lists.
 alist = [99, 'P', 1, 'xyz', 'a', 2.5]
 blist = ['xyz', 2.5, 2.5, 3, 99, 99, 2.5, 'xyz', -1.2, 99]
+
+# Solution 1
+clist1 = list(set(alist) & set(blist))
+print(f'List of common items: {clist1}')
