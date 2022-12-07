2460462aeb81d0f591bd9b40083fbec118556fcc
wooihaw <wooihaw.tan@gmail.com>
2022-12-07 15:51:45 +0800

    gitautosync update 20

--- exercises/string_exercises/03_rectangle.py
+++ exercises/string_exercises/03_rectangle.py
@@ -8,8 +8,8 @@
 try:
     length = float(input("Enter the length: "))
     width = float(input("Enter the width: "))
-except:
-    print('Invalid input. Please enter number only.')
+except Exception as e:
+    print(f'Error: {e}')
 else:
     perimeter = 2*length + 2*width
     area = length * width
