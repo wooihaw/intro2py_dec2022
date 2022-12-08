2451593bb952614e667e371601e5730a54fd2540
wooihaw <wooihaw.tan@gmail.com>
2022-12-08 14:21:51 +0800

    gitautosync update 10

--- examples/sys_argv.py
+++ examples/sys_argv.py
@@ -4,6 +4,7 @@ print(f"{sys.argv=}")
 if len(sys.argv) < 2:
     print("You need to provide an integer after the script name.")
     print("E.g.: python sys_argv.py 12")
+    sys.exit()
 
 num = int(sys.argv[1])
 print(num * "Hello ")
