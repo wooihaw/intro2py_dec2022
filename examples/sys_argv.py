1b3f873cd2d5400bf68fffcda062d8ff2ffc7057
wooihaw <wooihaw.tan@gmail.com>
2022-12-08 14:21:08 +0800

    gitautosync update 9

--- examples/sys_argv.py
+++ examples/sys_argv.py
@@ -1,5 +1,9 @@
 import sys
 print(f"{sys.argv=}")
 
+if len(sys.argv) < 2:
+    print("You need to provide an integer after the script name.")
+    print("E.g.: python sys_argv.py 12")
+
 num = int(sys.argv[1])
 print(num * "Hello ")
