d34e83e75f27ec740da2c9a25717def9be538e4c
wooihaw <wooihaw.tan@gmail.com>
2022-12-08 14:46:38 +0800

    gitautosync update 16

---
+++ examples/myprog2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 14:45:12 2022
+
+@author: wooihaw
+"""
+
+import mypackage.module1
+import mypackage.module2
+
+mypackage.module1.greet("Dave")
+mypackage.module2.depart("Alex")
