1b0e2f7521ff4e76ba2f4f0ef7ab04b3e8daa3d6
wooihaw <wooihaw.tan@gmail.com>
2022-12-08 17:06:06 +0800

    gitautosync update 27

---
+++ exercises/programming_challenges/challenge_10.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 17:00:15 2022
+
+@author: wooihaw
+"""
+from collections import Counter
+
+with open('data/alice.txt', 'r') as f:
+    s = f.read()
+    t = [c.lower() if c.isalpha() else ' ' for c in s]
+    w = ''.join(t).split()
+    
+    c = Counter(w)
+    print(f"Top 10 most frequent words: {c.most_common(10)}")
+    print(f"The word 'alice' appears {c['alice']} in the text.")
