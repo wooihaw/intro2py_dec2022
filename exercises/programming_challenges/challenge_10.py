7f307b0e8cd978033463dda517399e401a480d3d
wooihaw <wooihaw.tan@gmail.com>
2022-12-08 17:07:30 +0800

    gitautosync update 28

--- exercises/programming_challenges/challenge_10.py
+++ exercises/programming_challenges/challenge_10.py
@@ -8,8 +8,8 @@ from collections import Counter
 
 with open('data/alice.txt', 'r') as f:
     s = f.read()
-    t = [c.lower() if c.isalpha() else ' ' for c in s]
-    w = ''.join(t).split()
+    t = [c.lower() if c.isalpha() else ' ' for c in s] # replace non-alphabet with space
+    w = ''.join(t).split()  # w is the list of all words
     
     c = Counter(w)
     print(f"Top 10 most frequent words: {c.most_common(10)}")
