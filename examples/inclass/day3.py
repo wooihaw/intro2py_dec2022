0e3043633c363d2cf85f1511d1ed5616fc632646
wooihaw <wooihaw.tan@gmail.com>
2022-12-08 09:34:39 +0800

    gitautosync update 2

--- examples/inclass/day3.py
+++ examples/inclass/day3.py
@@ -4,3 +4,17 @@ Created on Thu Dec  8 09:28:43 2022
 
 @author: wooihaw
 """
+#%% Function example 1
+def add_one(x):
+    print(x + 1)
+
+add_one(5)
+x = add_one(3)
+print(f'{x=}')
+
+alist = [3, 1, 2]
+blist = alist.sort()
+clist = sorted(alist)
+print(f'{alist=}')
+print(f'{blist=}')
+print(f'{clist=}')
